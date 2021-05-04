395f7a74f91feaf28015d540521aac4ba384e2e4
Joao Cardoso <joao.cardoso@franq.com.br>
2021-05-04 12:29:06 -0300

    test ok

--- __tests__/simulateFootprint.test.js
+++ __tests__/simulateFootprint.test.js
@@ -3,6 +3,6 @@ const { Calculator } = require('../src/utils')
 const calculator = new Calculator()
 
 test('correct simulation is generated', () => {
-    expect(calculator.simulateFootprint({ electricity: 0, waste: 0, water: 0 })).toBe(0);
-    expect(calculator.simulateFootprint({ electricity: 10, waste: 10, water: 10 })).toBe(47190);
+    expect(calculator.simulateFootprint({ electricity: 0, vehicle: 0, waste: 0 })).toBe(0);
+    expect(calculator.simulateFootprint({ electricity: 10, vehicle: 10, waste: 10 })).toBe(1514.5014);
 });
